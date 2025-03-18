bf27d3c70e544c7a561a4830a91b0f35630376cc
lefarmer01600 <t.clemenceau@outlook.fr>
2025-03-18 09:57:34 +0100

    Remove unused room routes and controller; add join room page and update main app layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,29 @@
 "use client";
 
 import { useState } from "react";
+import Image from 'next/image';
 
 const App = () => {
   const [nickname, setNickname] = useState("AlQuaïda✌️");
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+    <div className="flex flex-col items-center justify-center min-h-screen p-6">
       {/* Header */}
       <header className="flex flex-col items-center mb-6">
-        {/* <img src="/bug-icon.png" alt="Bug Hunter Arena Logo" className="w-12 mb-2" /> */}
+        <Image src="/logo_bug_hunter.png" alt="Bug Hunter Arena Logo" width={48} height={48} className="w-12 mb-2" />
+        {/* <img src="/logo_bug_hunter.png" alt="Bug Hunter Arena Logo" className="w-12 mb-2" /> */}
         <h1 className="text-xl font-bold">Bug Hunter Arena</h1>
       </header>
 
       {/* Buttons */}
       <div className="w-full max-w-md flex flex-col gap-4">
         <a href="/join-room">
-          <button className="w-full py-3 text-lg font-semibold bg-yellow-400 rounded-lg shadow-md hover:bg-yellow-500">
+          <button className="w-full py-3 text-lg font-semibold rounded-2xl border-1 border-gray-300 hover:bg-gray-100">
             ➡ Rejoindre une Partie
           </button>
         </a>
         <a href="/create-room">
-          <button className="w-full py-3 text-lg font-semibold bg-white border-2 border-yellow-400 rounded-lg shadow-md hover:bg-yellow-100">
+          <button className="w-full py-3 text-lg font-semibold rounded-2xl border-1 border-gray-300 hover:bg-gray-100">
             ➕ Crée une Partie
           </button>
         </a>
@@ -47,6 +49,7 @@ const App = () => {
         {/* Instructions */}
         <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-md w-full md:w-1/2">
           <h2 className="text-lg font-bold text-gray-700 mb-2">Comment Jouer ?</h2>
+          <Image src="/Group 1.png" alt="" width={192} height={192}/>
           <p className="text-sm text-gray-600 text-center">
             Chaque équipe désigne un expert en PHP, JavaScript (ReactJS), C++, C# et mobile.
           </p>
